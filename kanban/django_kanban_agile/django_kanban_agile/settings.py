33c3f5d567d8777c9b51e1a6ed8a111e26b6a82e
ecrespo <ecrespo@gmail.com>
2015-05-01 18:49:58 -0430

    Agregada las aplicaciones de api rest full en settings.py

--- kanban/django_kanban_agile/django_kanban_agile/settings.py
+++ kanban/django_kanban_agile/django_kanban_agile/settings.py
@@ -31,12 +31,15 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = (
+    'material',
+    'material.admin',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'tastypie',
     'apps.kanban',
     'apps.backlog',
 )
