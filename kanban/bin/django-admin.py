0c5c178e83bb1a3b1f85393962ee43738f0ec5df
ecrespo <ecrespo@gmail.com>
2015-05-01 18:03:17 -0430

    Agregado Django en el virtualenv

---
+++ kanban/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/ecrespo/Copy/proyectos/fabrica/django-kanban/django_kanban-agile/kanban/bin/python2
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
